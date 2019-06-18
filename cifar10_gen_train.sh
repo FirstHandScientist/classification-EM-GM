91402cbdec562c24a9bfb0913a7855b40b4c3ea6
Dong Liu <doli@kth.se>
2019-06-18 19:19:55 +0200

    stable

--- cifar10_gen_train.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# python train.py hparams/GenMM/cifar101.json
-
-# python verify_acc.py results/cifar/k12hc64c1GenMM/cifar10.json
-
-python verify_acc.py results/cifar/k6hc128c1GenMM/cifar10.json &
-python verify_acc.py results/cifar/k6hc256c1GenMM/cifar10.json &
